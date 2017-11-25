478bcb82ca8b8d59e8157b6e92174e0995521d7c
XiaoxingCHen <cxx@seer-robotics.com>
2017-11-25 21:46:44 +0800

    Modify: make dir for f4kernel

---
+++ f4kerneltest/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+#include "udpIapDevice.h"
+#include <unistd.h>
+#include "iap.h"
+#include "stmflash.h"
+
+using namespace std;
+extern const uint8_t FIRMWARE_VERSION = 0x14;
+int main()
+{
+	cout <<"IAP simulator start!" << endl;
+    init_virtual_flash();
+	iapDevice.open();
+
+	printf("Firmware virsion: V%d.%d\r\n", FIRMWARE_VERSION>>4, FIRMWARE_VERSION&0xF);
+	while(1)
+	{
+		iapDevice.run();
+		iap_run();
+		usleep(1000);
+	}
+	return 0;
+}
